fa4e4ab0a76023f3bc4ed28b0baba265f31765c3
zhi.zhong <zhi.zhong@163.com>
2015-03-26 20:59:43 +0800

    Add uglifyJS

--- config/grunt/webpack.js
+++ config/grunt/webpack.js
@@ -1,11 +1,25 @@
 'use strict';
 
-var DEBUG = process.env.NODE_ENV === '';
+var webpack = require('webpack');
+
+var DEBUG = ['', 'dev'].indexOf(process.env.NODE_ENV) >= 0;
 var AUTOPREFIXER_LOADER = '!autoprefixer?{browsers:[' +
 '"Android 2.3", "Android >= 4", "Chrome >= 20", "Firefox >= 24", ' +
 '"Explorer >= 8", "iOS >= 6", "Opera >= 12", "Safari >= 6"]}';
 
 var minimize = DEBUG ? '' : '?minimize';
+
+var plugins = [];
+if (!DEBUG) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      //for node_modules
+      'warnings': false,
+      'drop_debugger': true,
+      'drop_console': true
+    }
+  }));
+}
 
 var config = {
   cache: DEBUG,
@@ -25,7 +39,7 @@ var config = {
     path: '.tmp/js',
     filename: 'main.js'
   },
-  plugins: [],
+  plugins: plugins,
   module: {
     preLoaders: [
       {
